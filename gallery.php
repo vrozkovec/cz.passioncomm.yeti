86041f1a24f0171b4fa7ca4b390f0e1b5d95cf45
vrozkovec <vit.rozkovec@gmail.com>
2014-02-17 13:46:49 +0100

    video, gallery

---
+++ gallery.php
@@ -0,0 +1,293 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+
+<?php
+	require "inc-head.php";
+?>
+
+<body>
+	<!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+
+<?php
+	require "inc-menu.php";
+?>
+
+	<div class="container info-blocks with-header">
+		<div class="padding20-left">
+
+
+			<div class="row">
+				<div class="col-xs-12 breadcrumb">
+					<span class="back"><i class="fa fa-angle-double-left"></i> <a
+						href="index.php">Back</a></span> <span class="path"><a href="#">Home</a>
+						/ <a href="#">Yetti Campaign Gallery</a></span>
+				</div>
+			</div>
+
+			<div class="row row-margin">
+				<div class="col-xs-12">
+					<div class="info-block text-leaf header">
+						<div class="orange-leaf-topshadow">
+							<div class="orange-leaf">
+								<div class="content standout">
+									<div class="wrapper">
+										<h2>Yetti Campaign</h2>
+										<div class="author">Benzina a. s.</div>
+									</div>
+								</div>
+								<div class="shadow"></div>
+							</div>
+						</div>
+						<img src="images/yeti@2x.png" />
+					</div>
+				</div>
+			</div>
+
+			<div class="row row-margin">
+				<div class="col-xs-12 text-content">
+					<h1>Project description</h1>
+					<p>Suspendisse auctor molestie orci et fermentum. Donec libero
+						mauris; varius id metus et, tempus volutpat tellus. Maecenas
+						semper sem massa, vel dictum arcu viverra id. Nam dictum magna sed
+						neque tincidunt, ac laoreet erat rutrum. Vivamus eget leo nisl.
+						Nunc varius vitae tortor nec posuere. Quisque mauris tortor,
+						placerat et sapien sed, pretium feugiat erat. Duis viverra, ante
+						ullamcorper auctor cursus, justo eros pharetra orci, eu bibendum
+						mi sem at sapien. Proin accumsan, dolor a ultricies rhoncus,
+						tortor justo auctor nunc, id facilisis arcu libero non metus.
+						Donec suscipit libero ante, vel pretium sem bibendum sed.
+						Suspendisse auctor at sapien vel malesuada? Curabitur a aliquam
+						neque, in vestibulum nibh. Maecenas sit amet semper tortor, ut
+						condimentum neque.</p>
+					<p>Vivamus cursus arcu felis, vel viverra nibh scelerisque non.
+						Vestibulum luctus ligula sit amet laoreet luctus. Suspendisse
+						dignissim, libero vitae vestibulum porttitor, neque sem rutrum
+						lectus, in tincidunt turpis eros eget dui. Ut auctor fringilla
+						libero eget venenatis. Mauris dignissim enim eu egestas
+						condimentum. Sed vel risus gravida, condimentum purus ac, laoreet
+						urna! Cras mollis eu massa vitae interdum. Aenean dictum ipsum non
+						sapien dictum interdum. Donec ultricies bibendum nunc vitae
+						auctor. Proin pellentesque massa ac vehicula dignissim?
+						Pellentesque pellentesque felis mi, quis fermentum nibh bibendum
+						at. In hac habitasse platea dictumst. Nunc placerat nisi congue
+						ultrices facilisis. Aenean non mi eu arcu suscipit vulputate ac
+						eget purus. Nullam non nunc in mi mattis fermentum.</p>
+				</div>
+			</div>
+
+			<div class="row row-margin gallery-row hidden-xs">
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img1.png" />
+					</a>
+				</div>
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img2.png" />
+					</a>
+				</div>
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img3.png" />
+					</a>
+				</div>
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img4.png" />
+					</a>
+				</div>
+			</div>
+
+			<div class="row row-margin gallery-row hidden-xs">
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img1.png" />
+					</a>
+				</div>
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img2.png" />
+					</a>
+				</div>
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img3.png" />
+					</a>
+				</div>
+				<div class="col-sm-3">
+					<a href="#"> <img src="images/gallery/img4.png" />
+					</a>
+				</div>
+			</div>
+
+			<div class="row row-margin visible-xs">
+				<div class="col-xs-12">
+					<div class="in-content-leaf">
+
+						<div class="orange-leaf">
+							<div class="content">
+								<a href="gallery.php">View Gallery<i class="fa fa-angle-double-right"></i></a>
+							</div>
+							<div class="shadow"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row row-margin visible-xs">
+				<div class="col-xs-12">
+					<div class="in-content-leaf">
+						<div class="orange-leaf">
+							<div class="content">
+								<a href="video.php">Watch the video<i class="fa fa-angle-double-right"></i></a>
+							</div>
+							<div class="shadow"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="row row-margin loader-row visible-xs">
+				<div class="col-xs-12">
+					<div class="in-content-leaf">
+						<div class="orange-leaf">
+							<div class="content">
+								<div>
+									<img src="css/img/preloader-white.gif" />
+								</div>
+								Load more work
+							</div>
+							<div class="shadow"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+
+			<div class="explore-more">
+				<h2>Explore more work</h2>
+				<div class="row row-margin">
+					<div class="col-lg-3 col-md-3 col-sm-3">
+						<div class="info-block text-leaf">
+
+							<div class="play-button">
+								<div class="text-center">
+									<a href="#"><i class="fa fa-play-circle-o"></i></a>
+								</div>
+							</div>
+
+							<div class="orange-leaf">
+								<div class="content">
+									<h2>Staropramen Alhemicar</h2>
+									<div class="author">Staropramen a. s.</div>
+									<a href="video.php">Watch the Video <i
+										class="fa fa-angle-double-right"></i></a>
+								</div>
+								<div class="shadow"></div>
+							</div>
+							<img src="images/block2@2x.png" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-3">
+						<div class="info-block full-orange-leaf">
+							<div class="orange-leaf">
+								<div class="content">
+									<h2 class="quote large">“Great creative ideas that help us
+										aachieving our international development goals with
+										Staropramen!”</h2>
+									<p>
+										<span class="quote-author">Frederic Landtmeters</span> CMO
+										Starbev
+									</p>
+								</div>
+								<div class="shadow"></div>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-3">
+						<div class="info-block text-leaf">
+							<div class="orange-leaf">
+								<div class="content active">
+									<h2>Welcome Julia our new designer</h2>
+									<div class="author">
+										<em>by</em> PassionComm
+									</div>
+									<a href="blog.php">Read More <i
+										class="fa fa-angle-double-right"></i></a>
+								</div>
+								<div class="shadow"></div>
+							</div>
+							<img src="images/block2@2x.png" />
+						</div>
+					</div>
+					<div class="col-lg-3 col-md-3 col-sm-3">
+						<div class="info-block text-leaf">
+
+							<div class="play-button">
+								<div class="text-center">
+									<a href="#"><i class="fa fa-play-circle-o"></i></a>
+								</div>
+							</div>
+
+							<div class="orange-leaf">
+								<div class="content">
+									<h2>Staropramen Alhemicar</h2>
+									<div class="author">Staropramen a. s.</div>
+									<a href="video.php">Watch the Video <i
+										class="fa fa-angle-double-right"></i></a>
+								</div>
+								<div class="shadow"></div>
+							</div>
+							<img src="images/block1@2x.png" />
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="col-lg-6 col-md-6 col-sm-6">
+						<div class="info-block text-leaf">
+
+							<div class="play-button">
+								<div class="text-center">
+									<a href="#"><i class="fa fa-play-circle-o"></i></a>
+								</div>
+							</div>
+
+							<div class="orange-leaf">
+								<div class="content">
+									<h2>The Art of Hard Rock event gallery</h2>
+									<div class="author">Hard Rock café</div>
+									<a href="video.php">Watch the Video <i
+										class="fa fa-angle-double-right"></i></a>
+								</div>
+								<div class="shadow"></div>
+							</div>
+							<img src="images/block3@2x.png" />
+						</div>
+					</div>
+					<div class="col-lg-6 col-md-6 col-sm-6">
+						<div class="info-block text-leaf">
+							<div class="orange-leaf">
+								<div class="content">
+									<h2>Holmes Place Opening Study</h2>
+									<div class="author">
+										<em>by</em> James Cook
+									</div>
+									<a href="blog.php">Read More <i
+										class="fa fa-angle-double-right"></i></a>
+								</div>
+								<div class="shadow"></div>
+							</div>
+							<img src="images/block3@2x.png" />
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	
+<?php
+	require "inc-footer.php";
+?>
+
+</body>
+</html>
