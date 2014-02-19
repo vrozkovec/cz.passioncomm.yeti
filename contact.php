d8b85230a9ebb20cd59a53c9c291e68524e7d929
vrozkovec <vit.rozkovec@gmail.com>
2014-02-19 08:36:29 +0100

    contact

--- contact.php
+++ contact.php
@@ -10,7 +10,7 @@
 	require "inc-head.php";
 ?>
 
-<body class="blog">
+<body class="blog contact">
 	<!--[if lt IE 7]>
             <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
         <![endif]-->
@@ -19,77 +19,87 @@
 	require "inc-menu.php";
 ?>
 
-	<div class="tophead">
-		<div class="container">
-			<div class="row">
-				<div class="col-xs-1 visible-xs arrow"><a href="#"><i class="fa fa-chevron-left"></i></a></div>
-				<div class="col-xs-10 col-sm-5 logo"></div>
-				<div class="col-xs-1 visible-xs arrow"><a href="#"><i class="fa fa-chevron-right"></i></a></div>
-				<div class="col-sm-7 hidden-xs slogan">
-					We promise that everything we do,<br /> we do with passion.
+
+	<div class="container info-blocks with-header">
+
+
+		<div class="row row-margin header">
+			<div class="col-xs-12 col-sm-8">
+				<div class="content-wrapper">
+					<div class="in-content-leaf">
+						<div class="orange-leaf">
+							<div class="content text-left">
+								<h1>Contact</h1>
+							</div>
+							<div class="shadow"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="tophead">
+			<div class="container">
+				<div class="row">
+					<div class="col-xs-1 visible-xs arrow">
+						<a href="#"><i class="fa fa-chevron-left"></i></a>
+					</div>
+					<div class="col-xs-10 col-sm-5 logo"></div>
+					<div class="col-xs-1 visible-xs arrow">
+						<a href="#"><i class="fa fa-chevron-right"></i></a>
+					</div>
+					<div class="col-sm-7 hidden-xs slogan">
+						We promise that everything we do,<br /> we do with passion.
+					</div>
 				</div>
 			</div>
 		</div>
 	</div>
 
 	<div class="container info-blocks with-header">
-		
-			<div class="row">
-				<div class="col-xs-12 breadcrumb">
-					<span class="back"><i class="fa fa-angle-double-left"></i> <a href="index.php">Back</a></span> <span class="path"><a href="#">Blog</a> / <a href="#">Contact</a></span>
-				</div>
-			</div>
-		
-						
-			<div class="row row-margin header">
-				<div class="col-xs-12 col-sm-8">
-					<div class="content-wrapper">
-						<div class="in-content-leaf">
-							<div class="orange-leaf">
-								<div class="content text-left">
-									<h1>Contact</h1>
-								</div>
-								<div class="shadow"></div>
-							</div>
-						</div>
-					</div>
-				</div>
-			</div>
 
-			<div class="row row-margin header">
-				<div class="col-xs-12">
-						<address>
-						  <span class="lead">+420 736 163 629</span><br>
-						  <a class="lead" href="contact@passioncomm.com">contact@passioncomm.com</a><br>
-						  <span class="lead">Passion Communications</span><br>
-						  Malostranské náměstí 266/5<br>
-						  Prague 1, Czech Republic<br>
-						</address>
-				</div>
+		<div class="row hidden-xs">
+			<div class="col-xs-12 breadcrumb">
+				<span class="back"><i class="fa fa-angle-double-left"></i> <a
+					href="index.php">Back</a></span> <span class="path"><a href="#">Blog</a>
+					/ <a href="#">Contact</a></span>
 			</div>
-		
-			<div class="row row-margin header">
-				<div class="col-xs-12">
-					<iframe width="100%" height="350" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.cz/maps?f=q&amp;source=s_q&amp;hl=cs&amp;geocode=&amp;q=Passion+Communications+s.r.o,+Praha+1&amp;aq=0&amp;oq=passion+communi&amp;sll=49.803763,15.474912&amp;sspn=5.439088,10.239258&amp;brcurrent=5,0,0&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;t=m&amp;ll=50.088649,14.402561&amp;spn=0.004819,0.00751&amp;z=16&amp;output=embed"></iframe></small>		
-				</div>
-			</div>
+		</div>
 
-			<div class="row row-margin socialicons">
-				<h2>
-					Follow Us!
-				</h2>
-				<div class="col-xs-12 text-center">
-					<a href="#" class="social google"><span>Share on google</span></a>
-					<a href="#" class="social facebook"><span>Share on facebook</span></a>
-					<a href="#" class="social twitter"><span>Share on twitter</span></a>
-					<a href="#" class="social email"><span>Share via e-mail</span></a>
-					<a href="#" class="social addthis"><span>Share with addthis</span></a>
+		<div class="row row-margin header">
+			<div class="col-xs-12">
+				<address>
+					<span class="lead">+420 736 163 629</span><br> <a class="lead"
+						href="contact@passioncomm.com">contact@passioncomm.com</a><br> <span
+						class="lead">Passion Communications</span><br> Malostranské
+					náměstí 266/5<br> Prague 1, Czech Republic<br>
+				</address>
+			</div>
+		</div>
+
+		<div class="row row-margin header">
+			<div class="col-xs-12">
+				<iframe width="100%" height="350" frameborder="0" scrolling="no"
+					marginheight="0" marginwidth="0"
+					src="https://maps.google.cz/maps?f=q&amp;source=s_q&amp;hl=cs&amp;geocode=&amp;q=Passion+Communications+s.r.o,+Praha+1&amp;aq=0&amp;oq=passion+communi&amp;sll=49.803763,15.474912&amp;sspn=5.439088,10.239258&amp;brcurrent=5,0,0&amp;ie=UTF8&amp;hq=&amp;hnear=&amp;t=m&amp;ll=50.088649,14.402561&amp;spn=0.004819,0.00751&amp;z=16&amp;output=embed"></iframe>
+				</small>
+			</div>
+		</div>
+
+		<div class="row row-margin socialicons inside">
+			<h2>Follow Us!</h2>
+			<div class="col-xs-12 text-center">
+				<div class="text-content">
+					<a href="#" class="social facebook"><i class="fa fa-facebook"></i></a>
+					<a href="#" class="social twitter"><i class="fa fa-twitter"></i></a>
+					<a href="#" class="social vimeo"><i class="fa fa-vimeo"></i></a>
 				</div>
-			</div>			
+			</div>
+		</div>
 	</div>
 	
 <?php
-	require "inc-footer.php";
+require "inc-footer.php";
 ?>
 
 </body>
