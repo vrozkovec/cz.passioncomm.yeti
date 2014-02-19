897bbd121c66aa4953322748b40cfec123f5ac7f
vrozkovec <vit.rozkovec@gmail.com>
2014-02-19 19:41:05 +0100

    test

---
+++ test.php
@@ -0,0 +1,267 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+<!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+<!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+<!--[if gt IE 8]><!-->
+<html class="no-js">
+<!--<![endif]-->
+<?php
+require "inc-head.php";
+?>
+
+<body>
+	<!--[if lt IE 7]>
+            <p class="chromeframe">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> or <a href="http://www.google.com/chromeframe/?redirect=true">activate Google Chrome Frame</a> to improve your experience.</p>
+        <![endif]-->
+<?php
+require "inc-menu.php";
+?>
+
+<?php
+require "inc-tophead.php";
+?>
+
+	<div class="container info-blocks">
+		<div class="row row-margin">
+			<div class="col-sm-8 col-md-6">
+				<!-- 
+				//dalsi moznost, jak elegantne vyresit zobrazovani obrazku
+				<div class="info-block text-leaf double-height" style="background: url('images/block0.png'); background-size: cover;">
+			 -->
+				<div class="info-block text-leaf double-height">
+					<div class="orange-leaf">
+						<div class="content standout">
+							<h2>
+							<form>
+								<input type="file" accept="image/*" capture="camera" multiple="multiple" />
+							</form>
+							
+							</h2>
+							<div class="author">Benzina a. s.</div>
+							<a href="blog.php">Read More <i class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/yeti-mobile.png" data-at2x="images/yeti-mobile@2x.png" class="visible-xs hires" width="500" height="500" />
+					<img src="images/yeti-tablet.png" data-at2x="images/yeti-tablet@2x.png" class="visible-sm hires" width="500" height="500" />
+					<img src="images/yeti-desktop.png" data-at2x="images/yeti-desktop@2x.png" class="visible-md visible-lg hires" width="500" height="500" />
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-3">
+				<div class="info-block text-leaf">
+
+					<div class="play-button">
+						<div class="text-center">
+							<a href="#"><i class="fa fa-play-circle-o"></i></a>
+						</div>
+					</div>
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>Ruce TV ad</h2>
+							<div class="author">Fortuna a. s.</div>
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block1@2x.png" />
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-3">
+				<div class="info-block full-orange-leaf">
+					<div class="orange-leaf">
+						<div class="content">
+							<h2 class="quote">“Working with Passion Comm was a delight.”</h2>
+							<p>
+								<span class="quote-author">Josh Gosh</span> Marketing Director
+								Kraft Foods
+							</p>
+						</div>
+						<div class="shadow"></div>
+					</div>
+				</div>
+			</div>
+
+
+			<div class="clearfix visible-sm"></div>
+
+
+			<div class="col-sm-4 col-md-3 hidden-md hidden-lg">
+				<div class="info-block text-leaf">
+					<div class="play-button">
+						<div class="text-center">
+							<a href="#"><i class="fa fa-play-circle-o"></i></a>
+						</div>
+					</div>
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>Staropramen Alhemicar</h2>
+							<div class="author">Staropramen a. s.</div>
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block2@2x.png" />
+				</div>
+			</div>
+			
+			<div class="col-sm-8 col-md-6">
+				<div class="info-block text-leaf">
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>How to Raise Conversion Rate by 30%</h2>
+							<div class="author">
+								<em>by</em> Lucy Morgan
+							</div>							
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/blog@2x.png" />
+				</div>
+			</div>
+			
+			<div class="clearfix visible-sm visible-lg"></div>
+			
+			
+
+			<div class="col-sm-4 col-md-3 hidden-xs hidden-sm">
+				<div class="info-block text-leaf">
+					<div class="play-button">
+						<div class="text-center">
+							<a href="#"><i class="fa fa-play-circle-o"></i></a>
+						</div>
+					</div>
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>Staropramen Alhemicar</h2>
+							<div class="author">Staropramen a. s.</div>
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block2@2x.png" />
+				</div>
+			</div>			
+			
+			<div class="col-sm-4 col-md-3">
+				<div class="info-block full-orange-leaf">
+					<div class="orange-leaf">
+						<div class="content">
+							<h2 class="quote large">“Great creative ideas that help us
+								aachieving our international development goals with
+								Staropramen!”</h2>
+							<p>
+								<span class="quote-author">Frederic Landtmeters</span> CMO
+								Starbev
+							</p>
+						</div>
+						<div class="shadow"></div>
+					</div>
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-3">
+				<div class="info-block text-leaf">
+					<div class="orange-leaf">
+						<div class="content active">
+							<h2>Welcome Julia our new designer</h2>
+							<div class="author">
+								<em>by</em> PassionComm
+							</div>
+							<a href="gallery.php">View gallery <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block2@2x.png" />
+				</div>
+			</div>
+			<div class="col-sm-4 col-md-3">
+				<div class="info-block text-leaf">
+
+					<div class="play-button">
+						<div class="text-center">
+							<a href="#"><i class="fa fa-play-circle-o"></i></a>
+						</div>
+					</div>
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>Staropramen Alhemicar</h2>
+							<div class="author">Staropramen a. s.</div>
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block1@2x.png" />
+				</div>
+			</div>
+
+						
+			<div class="clearfix visible-sm visible-lg visible-md"></div>
+			
+			<div class="col-sm-6 col-md-6">
+				<div class="info-block text-leaf">
+
+					<div class="play-button">
+						<div class="text-center">
+							<a href="#" class="invert"><i class="fa fa-play-circle-o"></i></a>
+						</div>
+					</div>
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>The Art of Hard Rock event gallery</h2>
+							<div class="author">Hard Rock café</div>
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block3@2x.png" />
+				</div>
+			</div>
+			
+			<div class="col-sm-6 col-md-6">
+				<div class="info-block text-leaf">
+
+					<div class="play-button">
+						<div class="text-center">
+							<a href="#" class="invert"><i class="fa fa-play-circle-o"></i></a>
+						</div>
+					</div>
+
+					<div class="orange-leaf">
+						<div class="content">
+							<h2>The Art of Hard Rock event gallery</h2>
+							<div class="author">Hard Rock café</div>
+							<a href="video.php">Watch the Video <i
+								class="fa fa-angle-double-right"></i></a>
+						</div>
+						<div class="shadow"></div>
+					</div>
+					<img src="images/block3@2x.png" />
+				</div>
+			</div>
+						
+		</div>
+	</div>
+	<!-- /container -->
+
+	<div class="container loader">
+		<img src="css/img/preloader.gif" /> Loading more...
+	</div>
+	
+<?php
+require "inc-footer.php";
+?>
+		</body>
+</html>
