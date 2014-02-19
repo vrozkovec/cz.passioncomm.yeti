561ff77b3196d16017c41a9d567ceee9b3ce5469
vrozkovec <vit.rozkovec@gmail.com>
2014-02-19 18:41:07 +0100

    retina

--- js/main.js
+++ js/main.js
@@ -35,6 +35,23 @@ $(document).ready(function() {
     	$('.blog .socialicons.outside').css('left', $('.container.with-header').offset().left - 50);
     }
     
+    if (window.devicePixelRatio == 2) {
+
+        var images = $("img.hires");
+
+        // loop through the images and make them hi-res
+        for(var i = 0; i < images.length; i++) {
+
+          // create new image name
+          var imageType = images[i].src.substr(-4);
+          var imageName = images[i].src.substr(0, images[i].src.length - 4);
+          imageName += "@2x" + imageType;
+
+          //rename image
+          images[i].src = imageName;
+        }
+    }
+        
 });
 
 $(window).resize(function() {
