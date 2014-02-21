e9a199ec6fe1e6e82214567101d7561aff52d7c3
vrozkovec <vit.rozkovec@gmail.com>
2014-02-21 12:40:08 +0100

    styles

--- inc-tophead.php
+++ inc-tophead.php
@@ -1,9 +1,9 @@
 	<div class="tophead">
 		<div class="container">
 			<div class="row">
-				<div class="col-xs-1 visible-xs arrow"><a href="#"><i class="fa fa-chevron-left"></i></a></div>
+				<div class="col-xs-1 visible-xs arrow"><a href="#"><i class="icon-arrow-left"></i></a></div>
 				<div class="col-xs-10 col-sm-5 logo"></div>
-				<div class="col-xs-1 visible-xs arrow"><a href="#"><i class="fa fa-chevron-right"></i></a></div>
+				<div class="col-xs-1 visible-xs arrow"><a href="#"><i class="icon-arrow-right"></i></a></div>
 				<div class="col-sm-7 hidden-xs slogan">
 					We promise that everything we do,<br /> we do with passion.
 				</div>
